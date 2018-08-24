52c0f5fa75583329fe4e2dd200c0e3b48a365d14
Suraj Kanojiya <suraj@Surajs-MacBook-Air.local>
2018-08-24 23:20:40 +0530

    home module updated

--- src/app/home/home-routing/home-routing.module.ts
+++ src/app/home/home-routing/home-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HomeComponent]
+  declarations: []
 })
-export class HomeModule { }
+export class HomeRoutingModule { }

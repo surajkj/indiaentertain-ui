836f9b73091a7828bdeb28819946e0b3b1abc57a
Suraj Kanojiya <suraj@Surajs-MacBook-Air.local>
2018-08-24 22:29:18 +0530

    auth module updated

--- src/app/auth/auth-routing/auth-routing.module.ts
+++ src/app/auth/auth-routing/auth-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AuthComponent]
+  declarations: []
 })
-export class AuthModule { }
+export class AuthRoutingModule { }

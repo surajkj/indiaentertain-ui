626f263f623cec013e2997e347cc3f81ca8c6c77
Suraj Kanojiya <suraj@Surajs-MacBook-Air.local>
2018-08-24 23:30:05 +0530

    setting module updated

--- src/app/setting/settings-routing/settings-routing.module.ts
+++ src/app/setting/settings-routing/settings-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SettingsComponent]
+  declarations: []
 })
-export class SettingModule { }
+export class SettingsRoutingModule { }
